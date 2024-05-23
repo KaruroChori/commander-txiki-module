3152db4706736073df55a46cb2ceb1503d147ae2
karurochari <nope>
2024-05-23 16:59:42 +0000

    removed unsupported features from txiki

--- dist/bundle/[module].js
+++ dist/bundle/[module].js
@@ -3,7 +3,6 @@
 class CommanderError extends Error {
   constructor(exitCode, code, message) {
     super(message);
-    Error.captureStackTrace(this, this.constructor);
     this.name = this.constructor.name;
     this.code = code;
     this.exitCode = exitCode;
@@ -14,7 +13,6 @@ class CommanderError extends Error {
 class InvalidArgumentError extends CommanderError {
   constructor(message) {
     super(1, "commander.invalidArgument", message);
-    Error.captureStackTrace(this, this.constructor);
     this.name = this.constructor.name;
   }
 }
